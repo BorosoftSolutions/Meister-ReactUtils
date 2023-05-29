818b433645df47b18397dc42fb38ad3d09bcab45
Marko Borojević <62252637+MarkoBorojevic@users.noreply.github.com>
2023-05-29 04:21:37 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 async function fetchWithTimeout(resource, options = {}) {
     const { timeout = 5000 } = options;
     
@@ -13,26 +14,43 @@ async function fetchWithTimeout(resource, options = {}) {
     return response;
 }
 
-module.exports = class API
+module.exports = class
 {
     constructor(serverAddress) {
         this.serverAddress = serverAddress;
     }
     async Get(route, params, headers) {
-        console.log("test");
         var r = route;
         if(params != null) r += "?" + new URLSearchParams(params);
         var object = {
             data: null,
             message: ""
         };
-        console.log(r);
+        console.log(this.serverAddress + route);
         try {
             const response = await fetch(this.serverAddress + r, { headers: headers});
-            object.data = await response.json();
+            const text = await response.text();
+
+            try {
+                object.data = JSON.parse(text);
+            } catch {
+                object.data = text;
+            }
+
         } catch (error) {
             object.message = error.message;
         } 
         return object;
+    }
+
+    async SignIn(username, password) {
+        return await this.Get("account/login", {username: username, password: password})
+    }
+
+    async GetAccountInfo(id) {
+        return await this.Get("account/get", { id: id})
+    }
+    async GetInfoFromToken(token) {
+        return await this.Get("account/get", null, {"auth-token": token.data});
     }
 }
